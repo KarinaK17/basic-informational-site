0942bd33f84dd35937a5b6e6e3d936aa76300765
KarinaK17 <117162205+KarinaK17@users.noreply.github.com>
2023-06-13 17:04:24 +0300

    rewrite using express

--- index.js
+++ index.js
@@ -1,36 +1,21 @@
-const http = require("http");
-const fs = require("fs");
+const express = require("express");
+const app = express();
+const port = 8080;
 
-const server = http.createServer((req, res) => {
-  res.setHeader("content-Type", "text/html");
-  let path = "./";
-  switch (req.url) {
-    case "/":
-      path += "index.html";
-      res.statusCode = 200;
-      break;
-    case "/about":
-      path += "about.html";
-      res.statusCode = 200;
-      break;
-    case "/contact-me":
-      path += "contact-me.html";
-      res.statusCode = 200;
-      break;
-    default:
-      path += "404.html";
-      res.statusCode = 404;
-      break;
-  }
+app.listen(port);
 
-  fs.readFile(path, (err, data) => {
-    if (err) {
-      console.log(err);
-      res.end();
-    } else {
-      res.end(data);
-    }
-  });
+app.get("/", (req, res) => {
+  res.sendFile("./index.html", { root: __dirname });
 });
 
-server.listen(8080);
+app.get("/contact-me", (req, res) => {
+  res.sendFile("./contact-me.html", { root: __dirname });
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile("./about.html", { root: __dirname });
+});
+
+app.use((req, res) => {
+  res.status(404).sendFile("./404.html", { root: __dirname });
+});
